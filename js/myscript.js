20ec213ac91c461f5cb8f5c4da2119e7961d0d4e
lukasikp <lukasik.paulina@gmail.com>
2015-11-04 21:03:56 +0100

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$("article a img").hover(function(){
+		$(this).fadeTo(500, "0.2");
+		$("a img").append("hfdhgf");
+	});
+
+var lista = document.getElementById("lista");
+
+	$("nav button").click(function(){
+	
+	
+});
+
